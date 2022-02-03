be4631e49b89ac38c780bea07196fd19f713508c
Ayberk Mogol <ayberk.m@yandex.com>
2022-02-04 01:11:14 +0300

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Ayberk Mogol on 4.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Ayberk Mogol on 4.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
