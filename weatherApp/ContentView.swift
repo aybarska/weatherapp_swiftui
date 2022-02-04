19074b8a110485008adb78f86b8e196b5c339d54
Ayberk Mogol <ayberk.m@yandex.com>
2022-02-04 16:51:48 +0300

    building landing page

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topTrailing, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topTrailing, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Istanbul, TR").font(.system(size: 32, weight: .medium, design: .default)).foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
         }
     }
 }
